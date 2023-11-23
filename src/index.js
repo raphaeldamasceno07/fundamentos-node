c1c0e454ae39be82e93a0db025ca6453ada4b37a
root <root@FISKAL12.fiskal.digital.local>
2023-11-23 15:07:58 -0300

    create develop

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ app.get("/courses", (request, response) => {
 });
 
 app.post("/courses", (request, response) => {
-  return response.json(["course 1", "course 2", "course 3","course 4"]);
+  return response.json(["course 1", "course 2", "course 3", "course 4"]);
 });
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["course 6", "course 5", "course 1", "course 4"]);
+})
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["course 6", "course 5", "course 1", "course 4"]);
+})
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["course 6", "course 5", "course 4"]);
+})
 
 app.listen(3333);
